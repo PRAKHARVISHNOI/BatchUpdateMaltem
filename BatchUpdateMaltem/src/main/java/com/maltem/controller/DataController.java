fe6a14aa9f146fb4e6844fd8cd68ea1ba413a250
PRAKHARVISHNOI <prakharvishnoi3@gmail.com>
2019-01-29 00:24:15 +0800

    commit

--- BatchUpdateMaltem/src/main/java/com/maltem/controller/DataController.java
+++ BatchUpdateMaltem/src/main/java/com/maltem/controller/DataController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "service/maltem")
+@RequestMapping(value = "service/maltem") 
 public class DataController {
-	
-	
-	
-	
-	
+
+
+
+
+
 
 }
